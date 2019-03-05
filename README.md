7700ad0aed4d6b676118b158452d19a0681c9c62
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2019-03-05 11:35:14 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Marathon JavaDriver Unit Testing Examples
+
+Collection of sample applications for using JavaDriver for unit testing
+Swing and Fx applications.
---
+++ swing-application/src/test/java/com/jaliansystems/javadriver/examples/swing/ut/LoginDialogTest.java
@@ -0,0 +1,97 @@
+package com.jaliansystems.javadriver.examples.swing.ut;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javax.swing.SwingUtilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import net.sourceforge.marathon.javadriver.JavaDriver;
+import net.sourceforge.marathon.javadriver.JavaProfile;
+import net.sourceforge.marathon.javadriver.JavaProfile.LaunchMode;
+import net.sourceforge.marathon.javadriver.JavaProfile.LaunchType;
+
+public class LoginDialogTest {
+
+	private LoginDialog login;
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		login = new LoginDialog() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onSuccess() {
+			}
+
+			@Override
+			protected void onCancel() {
+			}
+		};
+		SwingUtilities.invokeLater(() -> login.setVisible(true));
+		JavaProfile profile = new JavaProfile(LaunchMode.EMBEDDED);
+		profile.setLaunchType(LaunchType.SWING_APPLICATION);
+		driver = new JavaDriver(profile);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (login != null)
+			SwingUtilities.invokeAndWait(() -> login.dispose());
+		if (driver != null)
+			driver.quit();
+	}
+
+	@Test
+	public void loginSuccess() {
+		WebElement user = driver.findElement(By.cssSelector("text-field"));
+		user.sendKeys("bob");
+		WebElement pass = driver.findElement(By.cssSelector("password-field"));
+		pass.sendKeys("secret");
+		WebElement loginBtn = driver.findElement(By.cssSelector("button[text='Login']"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
+		loginBtn.click();
+		assertTrue(login.isSucceeded());
+	}
+
+	@Test
+	public void loginCancel() {
+		WebElement user = driver.findElement(By.cssSelector("text-field"));
+		user.sendKeys("bob");
+		WebElement pass = driver.findElement(By.cssSelector("password-field"));
+		pass.sendKeys("secret");
+		WebElement cancelBtn = driver.findElement(By.cssSelector("button[text='Cancel']"));
+		cancelBtn.click();
+		assertFalse(login.isSucceeded());
+	}
+
+	@Test
+	public void loginInvalid() throws InterruptedException {
+		WebElement user = driver.findElement(By.cssSelector("text-field"));
+		user.sendKeys("bob");
+		WebElement pass = driver.findElement(By.cssSelector("password-field"));
+		pass.sendKeys("wrong");
+		WebElement loginBtn = driver.findElement(By.cssSelector("button[text='Login']"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
+		loginBtn.click();
+		driver.switchTo().window("Invalid Login");
+		driver.findElement(By.cssSelector("button[text='OK']")).click();
+		driver.switchTo().window("Login");
+		user = driver.findElement(By.cssSelector("text-field"));
+		pass = driver.findElement(By.cssSelector("password-field"));
+		assertEquals("", user.getText());
+		assertEquals("", pass.getText());
+	}
+}
